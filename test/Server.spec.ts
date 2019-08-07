2414bd3c8aade6b8018f72261fc57338ebcbd196
Rilwan Adams <adamzrealone@gmail.com>
2019-08-07 23:31:50 +0100

    Restructure: all test files moved to a root folder

---
+++ test/Server.spec.ts
@@ -0,0 +1,41 @@
+import { expect, request, should, use } from 'chai';
+import chaiHTTP = require('chai-http');
+import App from '../src/app';
+import { PORT } from '../src/config';
+
+class Server {
+  constructor() {
+    this.build();
+  }
+
+  private req() {
+    return request(`localhost:${PORT}/api`);
+  }
+
+  private build() {
+    (() => new App().startApp())();
+    use(chaiHTTP);
+    should();
+    this.auth();
+  }
+
+  private auth() {
+    describe('AUTH ROUTES', () => {
+      // test route
+      it('GET /api/auth/test', (done) => {
+        this.req()
+          .get('/auth/test')
+          .end((err, res) => {
+            expect(err).to.be.equal(null);
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property('msg');
+            done();
+          });
+      });
+
+      // add other route under auth
+    });
+  }
+}
+
+(() => new Server())();
