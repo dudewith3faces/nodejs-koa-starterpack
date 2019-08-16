9d6ce96ea5355cf580ebb1d0e835b30de6c439a3
Rilwan Adams <adamzrealone@gmail.com>
2019-08-17 00:42:06 +0100

    Rename: file renamed

---
+++ src/routes/api/Auth.API.ts
@@ -0,0 +1,32 @@
+import * as Router from 'koa-router';
+import { AuthController } from '../controller';
+
+export default class AuthAPI {
+  private readonly router = new Router({ prefix: '/auth' });
+  constructor() {
+    this.get();
+    this.post();
+    this.delete();
+    this.route();
+  }
+
+  public route() {
+    return this.router.routes();
+  }
+
+  private get() {
+    /*
+     * @route  api/auth/test
+     * @desc   Test auth route
+     * */
+    this.router.get('/test', AuthController.test);
+  }
+
+  private post() {
+    // put post routes here
+  }
+
+  private delete() {
+    // put delete routes here
+  }
+}
