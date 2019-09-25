1a3960bbae5488350c86c9296417a63c7cb1d639
Rilwan Adams <adamzrealone@gmail.com>
2019-09-25 13:17:25 +0100

    Test: api test

--- test/Server.spec.ts
+++ test/Server.spec.ts
@@ -4,38 +4,38 @@ import App from '../src/app';
 import { PORT } from '../src/config';
 
 class Server {
-  constructor() {
-    this.build();
-  }
+	constructor() {
+		this.build();
+	}
 
-  private req() {
-    return request(`localhost:${PORT}/api`);
-  }
+	private req() {
+		return request(`localhost:${PORT}/api`);
+	}
 
-  private build() {
-    (() => new App().startApp())();
-    use(chaiHTTP);
-    should();
-    this.auth();
-  }
+	private build() {
+		(() => new App())();
+		use(chaiHTTP);
+		should();
+		this.auth();
+	}
 
-  private auth() {
-    describe('AUTH ROUTES', () => {
-      // test route
-      it('GET /api/auth/test', (done) => {
-        this.req()
-          .get('/auth/test')
-          .end((err, res) => {
-            expect(err).to.be.equal(null);
-            expect(res.status).to.equal(200);
-            expect(res.body).to.have.property('msg');
-            done();
-          });
-      });
+	private auth() {
+		describe('AUTH ROUTES', () => {
+			// test route
+			it('GET /api/auth/test', (done) => {
+				this.req()
+					.get('/auth/test')
+					.end((err, res) => {
+						expect(err).to.be.equal(null);
+						expect(res.status).to.equal(200);
+						expect(res.body).to.have.property('msg');
+						done();
+					});
+			});
 
-      // add other route under auth
-    });
-  }
+			// add other route under auth
+		});
+	}
 }
 
 (() => new Server())();
