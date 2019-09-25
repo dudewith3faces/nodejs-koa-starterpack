8152317bd924152298b5961c248217730412f80f
Rilwan Adams <adamzrealone@gmail.com>
2019-09-25 13:00:47 +0100

    SSL: ssl certificate option added

--- src/config/keys/server.ts
+++ src/config/keys/server.ts
@@ -1,10 +1,18 @@
-let env: string;
+import { readFileSync } from 'fs';
+import { ServerOptions } from 'https';
+import { join } from 'path';
 
-env = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'dev';
+let env = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'dev';
 env = env === 'development' ? 'dev' : env;
 
+const sslOpt: ServerOptions = {};
 const PORT = Number(process.env.PORT) || 5000;
 const hostname = process.env.hostname || '0.0.0.0';
 const origin = process.env.origin || 'https://locahost:4200';
 
-export { env, PORT, hostname, origin };
+if (env !== 'dev') {
+  sslOpt.cert = readFileSync(join(__dirname, '..', 'ssl', 'cert.pem'));
+  sslOpt.key = readFileSync(join(__dirname, '..', 'ssl', 'key.pem'));
+}
+
+export { env, PORT, hostname, origin, sslOpt };
